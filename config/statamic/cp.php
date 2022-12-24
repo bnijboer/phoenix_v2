de81968c0352e8e735eb8ebb8a361a00c09670bf
brendan.nijboer <brendan.nijboer@bakerware.nl>
2022-12-24 15:25:57 +0100

    update results partial + update config

--- config/statamic/cp.php
+++ config/statamic/cp.php
@@ -38,7 +38,12 @@ return [
     */
 
     'widgets' => [
-        'getting_started',
+//        'getting_started',
+        [
+            'type' => 'collection',
+            'collection' => 'blog',
+            'width' => 100
+        ],
     ],
 
     /*
