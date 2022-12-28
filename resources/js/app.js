c33e0f3eb97c3dba8f04e1e012ff3968dabed42d
brendan.nijboer <brendan.nijboer@bakerware.nl>
2022-12-28 21:53:19 +0100

    style sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { vfmPlugin } from 'vue-final-modal';
 import TheHeader from './components/layout/the-header';
-import TheSidebar from './components/layout/the-sidebar';
 import TheFooter from './components/layout/the-footer';
 import CommentSection from './components/comments/comment-section';
 
@@ -10,7 +9,6 @@ const pinia = createPinia();
 const app = createApp({});
 
 app.component('the-header', TheHeader)
-    .component('the-sidebar', TheSidebar)
     .component('the-footer', TheFooter)
     .component('comment-section', CommentSection);
 
